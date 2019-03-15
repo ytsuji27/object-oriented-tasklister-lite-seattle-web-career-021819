fb663085fb3323294986e765b513c1e5f730789b
Ian Candy <iancandysemail@gmail.com>
2019-03-15 12:08:24 -0400

    remove solution

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,14 +1,3 @@
 class TaskListItem {
-  constructor(description) {
-    this.description = description;
-  }
-
-  render() {
-    return `
-      <li>
-        ${this.description}
-        <button data-description="${this.description}">X</button>
-      </li>
-      `;
-  }
+  
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // initialize taskList class
   const taskList = new TaskList();
-  //grab all the necessary DOM elements
 
-  //form and relevant input fields
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
   const listContainer = document.getElementById("list");
-
   const renderApp = () => (listContainer.innerHTML = taskList.render());
-  //attach event listeners
 
-  newTaskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    taskList.createNewTask(newTaskDescription.value);
-    // reset form
-    e.target.reset();
-    renderApp();
-  });
-
-  listContainer.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      taskList.deleteTask(e.target.dataset.description);
-      renderApp();
-    }
-  });
+  // Add Form Behavior Here!
 
   renderApp()
 });
